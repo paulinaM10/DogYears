ed6757c59ff7fdf1a6b38feafe83bfaf69977f8b
Paulina Malek <paulina302aa@gmail.com>
2022-06-08 18:11:12 +0100

    commit

---
+++ dog.js
@@ -0,0 +1,16 @@
+//create variable with my age
+let myAge = 26;
+myAge = 27;
+//will chage it
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears = laterYears * 4;
+console.log(earlyYears);
+console.log(laterYears);
+//comment
+myAgeInDogYears = earlyYears + laterYears;
+myName = "Paulina".toLowerCase();
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
